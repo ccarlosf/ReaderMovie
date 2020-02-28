427341362d700a50fc51afe5b761929a5e21e697
ccarlos <2238513100@qq.com>
2020-02-28 22:57:20 +0800

    4-11 条件渲染与列表渲染
    4-11 条件渲染与列表渲染

--- pages/posts/posts.js
+++ pages/posts/posts.js
@@ -6,7 +6,10 @@ Page({
    */
   data: {
     date: 'Nov  18  2020',
-    title: '正是虾肥蟹壮时'
+    title: '正是虾肥蟹壮时',
+    posts_key:[
+
+    ]
     // 单向数据绑定 自动
     // 双向数据绑定
   },
@@ -25,18 +28,28 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-    var post_content1 = {
+    var posts_content = [{
       date: "Sep 18 2016",
       title: "正是虾肥蟹壮时",
-      img:{
-        post_img: "/images/post/crab.png",
-        author_img: '/images/avatar/1.png'
-      },
+      post_img: "/images/post/crab.png",
+      author_img: '/images/avatar/1.png',
       content: "菊黄蟹正肥，品尝秋之味。徐志摩把,“看初花的荻芦”和“到楼外楼吃蟹”,并列为秋天来杭州不能错过的风雅之事；用林妹妹的话讲是“螯封嫩玉双双满，",
       view_num: '112',
-      collect_num: '96',
-    }
-    this.setData(post_content1)
+      collect_num: '96'
+    }, {
+      date: "Nov 20 2016",
+      title: "比利·林恩的中场故事",
+      post_img: "/images/post/bl.png",
+      author_img: "/images/avatar/2.png",
+      content: "一 “李安是一位绝不会重复自己的导演，本片将极富原创性李安众所瞩目的新片《比利林恩漫长的中场休息》，正式更名《半场无战事》。",
+      view_num: '96',
+      collect_num: '112',
+    }]
+
+
+    this.setData({
+      posts_key:posts_content
+      })
   },
 
   /**
