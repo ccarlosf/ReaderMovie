8be0c9dab21eafdb84021b75f92a12e44aa8b73a
ccarlos <2238513100@qq.com>
2020-02-28 23:15:33 +0800

    4-12 小程序的事件机制——捕捉与回调
    4-12 小程序的事件机制——捕捉与回调

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -1,3 +1,20 @@
 Page({
-  
+  onTap: function() {
+      wx.navigateTo({
+        url: '../posts/posts'
+      })
+
+    // wx.redirectTo({
+    //   url: '../posts/posts',
+    // })
+        // console.log('onTap')
+  },
+
+  onUnload:function(){
+    console.log('welcome page is unload')
+  },
+
+  onHide:function(){
+    console.log('welcome page is onHide')
+  }
 })
