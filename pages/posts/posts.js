292dd416c4c1c256bd0d1a01cb337ef918fe1b81
ccarlos <2238513100@qq.com>
2020-02-28 22:04:11 +0800

    4-9 setData方法绑定数据
    4-9 setData方法绑定数据

--- pages/posts/posts.js
+++ pages/posts/posts.js
@@ -25,7 +25,16 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
-
+    var post_content1 = {
+      date: "Sep 18 2016",
+      title: "正是虾肥蟹壮时",
+      post_img: "/images/post/crab.png",
+      content: "菊黄蟹正肥，品尝秋之味。徐志摩把,“看初花的荻芦”和“到楼外楼吃蟹”,并列为秋天来杭州不能错过的风雅之事；用林妹妹的话讲是“螯封嫩玉双双满，",
+      view_num: '112',
+      collect_num: '96',
+      author_img: '/images/avatar/1.png'
+    }
+    this.setData(post_content1)
   },
 
   /**
