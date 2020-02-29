08ca8ed9f9405f890322ac71e27dc3b7601c5e4f
ccarlos <2238513100@qq.com>
2020-02-29 22:05:50 +0800

    4-13 小程序的事件机制——catch与bind
    4-13 小程序的事件机制——catch与bind

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -1,20 +1,30 @@
 Page({
   onTap: function() {
-      wx.navigateTo({
-        url: '../posts/posts'
-      })
+      // wx.navigateTo({
+      //   url: '../posts/posts'
+      // })
 
-    // wx.redirectTo({
-    //   url: '../posts/posts',
-    // })
-        // console.log('onTap')
+    wx.redirectTo({
+      url: '../posts/posts',
+    })
+
+    
+    // 冒泡和非冒泡
   },
 
+  // onCatainerTap:function(){
+  //   console.log('Catainer Tap')
+  // },
+
+  // onSubTap:function(){
+  //   console.log('Sub Tap')
+  // },
+
   onUnload:function(){
-    console.log('welcome page is unload')
+    // console.log('welcome page is unload')
   },
 
   onHide:function(){
-    console.log('welcome page is onHide')
+    // console.log('welcome page is onHide')
   }
 })
