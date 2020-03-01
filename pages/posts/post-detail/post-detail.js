ee8900ee607ed041e2644368069fc644cd0911f1
ccarlos <2238513100@qq.com>
2020-03-01 16:51:34 +0800

    6-5 缓存Storage的基本用法

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -7,5 +7,27 @@ Page({
         this.setData({
             postData: postData
         })
+
+        // wx.setStorageSync('key',"风暴英雄")
+        wx.setStorageSync('key', {
+            game: "风暴英雄",
+            developer: "暴雪"
+        })
+
+        wx.setStorageSync('key1', {
+            game: "LOL",
+            developer: "拳头"
+        })
+    },
+
+    onCollectionTap:function (event) {
+       var game = wx.getStorageSync('key');
+        console.log(game)
+    },
+
+    onShareTap:function (event) {
+        // wx.removeStorageSync('key')
+        // 缓存的上限最大不能超过10M
+        wx.clearStorageSync();
     }
 })
