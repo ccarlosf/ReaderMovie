0fb4070a138b58cc72c1153a93c6b191c5484f5b
ccarlos <2238513100@qq.com>
2020-03-01 19:51:25 +0800

    6-10 交互反馈 wx.showActionSheet

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -3,14 +3,14 @@ var postsData = require('../../../data/posts-data')
 Page({
     data: {},
     onLoad: function (option) {
-        console.log("微信小程序")
+        // console.log("微信小程序")
         var postId = option.id;
         this.data.currentPostId = postId;
         var postData = postsData.postList[postId];
         this.setData({
             postData: postData
         })
-        wx.setStorageSync('key', '英雄联盟')
+        // wx.setStorageSync('key', '英雄联盟')
         var postsCollected = wx.getStorageSync('posts_collected')
         if (postsCollected) {
             var postCollected = postsCollected[postId]
@@ -32,21 +32,21 @@ Page({
         // 收藏变成未收藏，未收藏变成收藏
         postCollected = !postCollected;
         postsCollected[this.data.currentPostId] = postCollected;
-        this.showToast(postsCollected,postCollected);
+        this.showToast(postsCollected, postCollected);
     },
 
-    showModal:function(postsCollected,postCollected){
+    showModal: function (postsCollected, postCollected) {
         var that = this;
         wx.showModal({
             title: "收藏",
-            content:postCollected?"收藏该文章?":"取消收藏该文章?",
-            showCancel:"true",
-            cancelText:"取消",
-            cancelColor:"#333",
-            confirmText:"确认",
-            confirmColor:"#405f80",
-            success:function (res) {
-                if(res.confirm){
+            content: postCollected ? "收藏该文章?" : "取消收藏该文章?",
+            showCancel: "true",
+            cancelText: "取消",
+            cancelColor: "#333",
+            confirmText: "确认",
+            confirmColor: "#405f80",
+            success: function (res) {
+                if (res.confirm) {
                     // 更新文章是否的缓存值
                     wx.setStorageSync('posts_collected', postsCollected);
                     // 更新数据绑定变量，从而实现切换图片
@@ -58,7 +58,7 @@ Page({
         })
     },
 
-    showToast:function (postsCollected,postCollected) {
+    showToast: function (postsCollected, postCollected) {
         // 更新文章是否的缓存值
         wx.setStorageSync('posts_collected', postsCollected);
         // 更新数据绑定变量，从而实现切换图片
@@ -68,8 +68,28 @@ Page({
         wx.showToast({
             title: postCollected ? "收藏成功" : "取消成功",
             duration: 1000,
-            icon:"success"
+            icon: "success"
         })
+    },
 
+    onShareTap: function (event) {
+        var itemList = [
+            "分享给微信好友",
+            "分享到朋友圈",
+            "分享到QQ",
+            "分享到微博"
+        ];
+        wx.showActionSheet({
+            itemList: itemList,
+            itemColor: "#405f80",
+            success: function (res) {
+                // res.cancel 用户是不是点击了取消按钮
+                // res.tapIndex 数组元素的序号,从0开始
+                wx.showModal({
+                    title: "用户"+itemList[res.tapIndex],
+                    content: "用户是否取消?"+res.cancel+"现在无法实现分享功能，什么时候能支持呢？"
+                })
+            }
+        })
     }
 })
