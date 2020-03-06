84a2269d6f89ee9ceb6ca898f608db48aa1c7e16
ccarlos <2238513100@qq.com>
2020-03-06 21:47:36 +0800

    6-15 监听播放事件完善音乐播放

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -26,6 +26,19 @@ Page({
             postsCollected[postId] = false;
             wx.setStorageSync('posts_collected', postsCollected)
         }
+
+        var that = this;
+        wx.onBackgroundAudioPlay(function () {
+            that.setData({
+                isPlayingMusic: true
+            })
+        });
+        wx.onBackgroundAudioPause(function () {
+            that.setData({
+                isPlayingMusic: false
+            })
+        })
+
     },
 
     onCollectionTap: function (event) {
